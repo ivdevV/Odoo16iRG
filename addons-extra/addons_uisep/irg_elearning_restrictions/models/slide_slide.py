cd1fecf7b7b6746b98c1647b28a6d3b60039ea5d
ivrogo <er_jotx@hotmail.com>
2025-12-16 18:59:12 +0100

    restored original implementation

--- addons-extra/addons_uisep/irg_elearning_restrictions/models/slide_slide.py
+++ addons-extra/addons_uisep/irg_elearning_restrictions/models/slide_slide.py
@@ -4,9 +4,15 @@ from odoo.exceptions import AccessError
 class Slide(models.Model):
     _inherit = 'slide.slide'
 
-    # Usamos un campo Char simple para descartar problemas de relaciones
-    test_visibility_field = fields.Char(
-        string='Campo de Prueba (Visible?)',
-        default='Si ves esto, el módulo funciona'
+    restriction_slide_id = fields.Many2one(
+        'slide.slide',
+        string='Diapositiva Requisito (Test)',
+        help='La diapositiva que debe completarse antes de acceder a esta.'
     )
+
+    def _check_prerequisite(self):
+        """ Override or extend this if we were implementing the full check logic.
+            For now, we just want to see the field.
+        """
+        pass
 
