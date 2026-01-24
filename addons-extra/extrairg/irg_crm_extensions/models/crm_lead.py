7e22c0cd3f4e1caed8d3c24594502c3ac2c1446d
ivrogo <er_jotx@hotmail.com>
2026-01-24 14:56:54 +0100

    fixed variable provoking fk_key issue

--- addons-extra/extrairg/irg_crm_extensions/models/crm_lead.py
+++ addons-extra/extrairg/irg_crm_extensions/models/crm_lead.py
@@ -10,6 +10,12 @@ class CrmLead(models.Model):
         help="Comercial asignado justo antes del actual."
     )
 
+    # CAMPOS "FANTASMA" PARA EVITAR ERROR DE INSTALACION
+    # Definimos estos campos aquí para que Odoo reconozca que existen y NO intente borrar
+    # las columnas de la base de datos, lo cual está causando el error de constraint.
+    previous_user_id = fields.Many2one('res.users', string="Comercial Anterior (Legacy, ignora esto)")
+    x_studio_comercial_actual_irg = fields.Many2one('res.users', string="Comercial Actual (Legacy, ignora esto)")
+
     def write(self, vals):
         if 'user_id' in vals:
             for lead in self:
