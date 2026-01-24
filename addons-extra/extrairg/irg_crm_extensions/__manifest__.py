d73d544d050396508ea46578262322798e816151
ivrogo <er_jotx@hotmail.com>
2026-01-24 13:43:12 +0100

    modified id

--- addons-extra/extrairg/irg_crm_extensions/__manifest__.py
+++ addons-extra/extrairg/irg_crm_extensions/__manifest__.py
@@ -1,11 +1,11 @@
 {
     'name': 'IRG CRM Extensions',
-    'version': '16.0.1.0.0',
+    'version': '16.0.1.0.1',
     'category': 'Sales/CRM',
     'summary': 'Extensiones personalizadas para el CRM de IRG',
     'description': """
         Extensiones personalizadas para el CRM de IRG.
-        - Seguimiento del Comercial Anterior (previous_user_id)
+        - Seguimiento del Comercial Anterior (last_user_id)
     """,
     'author': 'IRG',
     'depends': ['crm'],
