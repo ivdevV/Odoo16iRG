055087f761923e33c9162de5ddf08a56bcdd68b5
ivdevV <soporte@institutoraimongaja.com>
2026-02-18 15:55:40 +0100

    añadido variable para especificar el curso

--- addons-extra/extrairg/irg_forum_batch_visibility/models/forum_forum.py
+++ addons-extra/extrairg/irg_forum_batch_visibility/models/forum_forum.py
@@ -12,3 +12,11 @@ class ForumForum(models.Model):
         string='Visibility Batches',
         help='If empty, all users can access this forum. If set, only users in these batches can access it.',
     )
+    visibility_course_ids = fields.Many2many(
+        'op.course',
+        'forum_forum_visibility_course_rel',
+        'forum_id',
+        'course_id',
+        string='Visibility Courses',
+        help='Courses linked to this forum (multiple selection).',
+    )
