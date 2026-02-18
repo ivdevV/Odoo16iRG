daabc7f8cfeb5bac3071fa19332c1606225c770e
ivrogo <er_jotx@hotmail.com>
2026-02-18 19:44:18 +0100

    fix 2: solucionado problema de permiso para visualizar el foro en el perfil del campus

--- addons-extra/extrairg/irg_campus_course_forum/controllers/main.py
+++ addons-extra/extrairg/irg_campus_course_forum/controllers/main.py
@@ -11,19 +11,35 @@ class DashboardPortalCampusForum(DashboardPortal):
         response = super().view_user_profile_course(course_id, **post)
 
         course = request.env['op.course'].browse(course_id)
-        forum = course.forum_id if course.exists() else request.env['forum.forum']
+        forum_ids = request.env['forum.forum']
         post_ids = request.env['forum.post']
+        posts_by_forum_id = {}
 
-        if forum:
-            post_ids = request.env['forum.post'].search([
-                ('forum_id', '=', forum.id),
-                ('parent_id', '=', False),
-            ], order='create_date desc')
+        if course.exists():
+            forum_ids = request.env['forum.forum'].search([
+                ('visibility_course_ids', 'in', course.id),
+            ], order='name asc')
+
+            if course.forum_id:
+                forum_ids |= course.forum_id
+                forum_ids = forum_ids.sorted(key=lambda forum: forum.name or '')
+
+            if forum_ids:
+                post_ids = request.env['forum.post'].search([
+                    ('forum_id', 'in', forum_ids.ids),
+                    ('parent_id', '=', False),
+                ], order='create_date desc')
+                posts_by_forum_id = {
+                    forum.id: post_ids.filtered(lambda forum_post: forum_post.forum_id == forum)
+                    for forum in forum_ids
+                }
 
         if hasattr(response, 'qcontext'):
             response.qcontext.update({
-                'course_forum': forum,
+                'course_forum': forum_ids[:1],
+                'course_forum_ids': forum_ids,
                 'course_forum_post_ids': post_ids,
+                'course_forum_posts_map': posts_by_forum_id,
             })
 
         return response
