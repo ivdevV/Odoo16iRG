56d45558d226b8d26d05fdcf36f779aba4a3cd57
ivdevV <soporte@institutoraimongaja.com>
2026-02-18 14:45:57 +0100

    añadida pestaña para controlar la visibilidad de cada foro por lote

--- addons-extra/extrairg/irg_forum_batch_visibility/models/forum_post.py
+++ addons-extra/extrairg/irg_forum_batch_visibility/models/forum_post.py
@@ -1,5 +1,14 @@
-from odoo import models
+from odoo import fields, models
 
 
 class ForumPost(models.Model):
     _inherit = 'forum.post'
+
+    visibility_batch_ids = fields.Many2many(
+        'forum.visibility.batch',
+        'forum_post_visibility_batch_rel',
+        'post_id',
+        'batch_id',
+        string='Visibility Batches',
+        help='If empty, this post follows the forum batch rules. If set, only users in these batches can read this post.',
+    )
