841e26c26ea39e4a9db2e5c3345faef631c667d1
Sebastian <soporte2@institutoraimongaja.com>
2026-02-20 10:46:45 +0100

    Fix forum auth rules using effective admission batches

--- addons-extra/extrairg/irg_forum_batch_visibility/models/res_users.py
+++ addons-extra/extrairg/irg_forum_batch_visibility/models/res_users.py
@@ -12,3 +12,19 @@ class ResUsers(models.Model):
         string='Forum Allowed Batches',
         help='Batches that this user can access in forum posts and forums.',
     )
+
+    forum_effective_batch_ids = fields.Many2many(
+        'op.batch',
+        compute='_compute_forum_effective_batch_ids',
+        string='Forum Effective Batches',
+        help='Union of direct forum batches and batches from admissions for this user.',
+    )
+
+    def _compute_forum_effective_batch_ids(self):
+        Admission = self.env['op.admission'].sudo()
+        for user in self:
+            admission_batch_ids = Admission.search([
+                ('partner_id', '=', user.partner_id.id),
+                ('batch_id', '!=', False),
+            ]).mapped('batch_id')
+            user.forum_effective_batch_ids = user.op_batch_ids | admission_batch_ids
