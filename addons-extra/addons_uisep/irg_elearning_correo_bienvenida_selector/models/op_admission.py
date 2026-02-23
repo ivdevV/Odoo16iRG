1acd43aa84404b45b4caa394024e0c18a7fe3ea2
ivdevV <soporte@institutoraimongaja.com>
2026-02-23 14:04:01 +0100

    restaurada logica original del modulo

--- addons-extra/addons_uisep/irg_elearning_correo_bienvenida_selector/models/op_admission.py
+++ addons-extra/addons_uisep/irg_elearning_correo_bienvenida_selector/models/op_admission.py
@@ -21,21 +21,7 @@ class OpAdmission(models.Model):
             if self.batch_id.modality_id:
                 modality_name = self.batch_id.modality_id.name.lower()
                 if 'online' in modality_name:
-                    configured_id = self.env['ir.config_parameter'].sudo().get_param(
-                        'irg_elearning_correo_bienvenida_selector.online_template_id'
-                    )
-                    template = False
-                    if configured_id and str(configured_id).isdigit():
-                        template = self.env['mail.template'].sudo().browse(int(configured_id))
-                        if not template.exists():
-                            template = False
-                    if not template:
-                        template = self.env.ref(
-                            'irg_elearning_correo_bienvenida_selector.email_op_admission_confirm_online',
-                            raise_if_not_found=False,
-                        )
-                    if template:
-                        template_id = template.id
+                    template_id = self.env.ref('irg_elearning_correo_bienvenida_selector.email_op_admission_confirm_online').id
             
             self.with_context(force_send=force).message_post_with_template(template_id, email_layout_xmlid=False)
             self.email_send_ok = True
