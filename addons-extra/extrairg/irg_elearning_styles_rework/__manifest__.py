50fcce40d581719b933d3d5f4041bf7ffba49ec4
ivdevV <soporte@institutoraimongaja.com>
2026-03-04 13:32:34 +0100

    creado modulo para revamp de los estilos dentro del modulo de elearning

---
+++ addons-extra/extrairg/irg_elearning_styles_rework/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'IRG eLearning Styles Rework',
+    'version': '16.0.1.0.0',
+    'summary': 'Rework visual moderno para páginas de eLearning',
+    'category': 'Website/eLearning',
+    'author': 'iRG',
+    'license': 'LGPL-3',
+    'depends': [
+        'website_slides',
+        'website',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/website_slides_rework.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'irg_elearning_styles_rework/static/src/scss/irg_elearning_styles_rework.scss',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
