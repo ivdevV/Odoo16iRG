6ed0528392ea983af2e9da8a8428798ec03defe9
ivdevV <soporte@institutoraimongaja.com>
2026-03-06 11:50:57 +0100

    feat(forum): add module to disable karma requirements

---
+++ addons-extra/extrairg/irg_forum_disable_karma/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "iRG Forum Disable Karma",
+    "summary": "Disable all forum karma requirements and scoring",
+    "version": "16.0.1.0.0",
+    "category": "Website/Forum",
+    "author": "iRG Developer",
+    "license": "AGPL-3",
+    "depends": ["website_forum"],
+    "data": [],
+    "post_init_hook": "post_init_hook",
+    "installable": True,
+    "application": False,
+}
