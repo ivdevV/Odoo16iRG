26a9696e61bf8e099d41798f9fa8c3f7386b9b98
Sebastian <soporte2@institutoraimongaja.com>
2026-03-09 12:47:44 +0100

    fix(irg_exam_score_100): corregir manifest duplicado y eliminar vistas no necesarias

--- addons-extra/extrairg/irg_exam_score_100/__manifest__.py
+++ addons-extra/extrairg/irg_exam_score_100/__manifest__.py
@@ -5,27 +5,10 @@
     'category': 'Tools',
     'author': 'iRG Inc',
     'license': 'LGPL-3',
-    'depends': ['survey'],
+    'depends': [
+        'survey',
+    ],
     'data': [],
     'installable': True,
     'auto_install': False,
-}
-{
-    'name': 'iRG Exam Score 100',
-    'summary': 'Normaliza examenes/certificaciones survey a escala 0-100',
-    'version': '16.0.1.0.0',
-    'category': 'Website/Survey',
-    'author': 'iRG',
-    'license': 'LGPL-3',
-    'depends': [
-        'survey',
-        'website_slides_survey',
-        'isep_survey',
-        'isep_gradebook',
-    ],
-    'data': [
-        'views/survey_survey_views.xml',
-    ],
-    'installable': True,
-    'application': False,
 }
