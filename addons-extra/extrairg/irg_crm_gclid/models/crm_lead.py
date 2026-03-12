31a16ba9ee91b7adc079e9f77ac734bb0b343ba8
Sebastian <soporte2@institutoraimongaja.com>
2026-03-12 12:33:36 +0100

    Revert "chore: merge feat/irg_crm_gclid into Dev_iRG"
    This reverts commit 8c235d142a5c12a7e0b1df840e535f79b75b4cd6, reversing
    changes made to f96c32b924a34becff2e070755ed1465bcfdb704.

--- addons-extra/extrairg/irg_crm_gclid/models/crm_lead.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-
-class CrmLead(models.Model):
-    _inherit = 'crm.lead'
-
-    x_gclid = fields.Char(string='GCLID', help='Google Click ID (GCLID) tracking field', copy=False)
