dfe52ed92559de5227c68cf814308ca5a853db63
ivdevV <soporte@institutoraimongaja.com>
2026-03-17 11:58:22 +0100

    modificada logica de la firma

--- addons-extra/addons_uisep/isep_ecommerce_fix/static/src/components/website_sale_payment_inherit/website_sale_payment_inherit.js
+++ addons-extra/addons_uisep/isep_ecommerce_fix/static/src/components/website_sale_payment_inherit/website_sale_payment_inherit.js
@@ -6,8 +6,9 @@ odoo.define('isep_ecommerce_fix.signature_check', function (require) {
 
     const SignatureCheckMixin = {
         start: function () {
-            this.$signatureCheckbox = this.$('#checkbox_signed');
+            this.$signButton = this.$('#sign_button');
             this.$orderIdInput = this.$('#order_id_holder');
+            this.$signatureSigned = this.$('#signature_signed');
             this.$submitButton = this.$('button[name="o_payment_submit_button"]');
 
             this._checkInitialSignature();
@@ -16,7 +17,7 @@ odoo.define('isep_ecommerce_fix.signature_check', function (require) {
         },
 
         _checkInitialSignature: function () {
-            if (!this.$signatureCheckbox.length) return;
+            if (!this.$signButton.length) return;
 
             const orderId = this._getOrderId();
             if (!orderId) return;
@@ -27,13 +28,9 @@ odoo.define('isep_ecommerce_fix.signature_check', function (require) {
             }).then(data => {
                 this._setSignLink(data.sign_link || '');
                 if (data.signed) {
-                    this.$signatureCheckbox.prop('checked', true);
-                    $('#signature_warning').hide();
-                    $('#sign_button').hide();
+                    this._markAsSigned();
                 } else {
-                    this.$signatureCheckbox.prop('checked', false);
-                    $('#signature_warning').hide();
-                    $('#sign_button').hide();
+                    this._markAsUnsigned();
                 }
                 this._updateSignatureDisabledReason();
             }).catch(() => {
@@ -42,35 +39,17 @@ odoo.define('isep_ecommerce_fix.signature_check', function (require) {
             });
         },
 
-        _onClickSignatureCheckbox: function () {
-            const orderId = this._getOrderId();
-            if (!orderId) return;
+        _markAsSigned: function () {
+            this.$signatureSigned.val('1');
+            $('#sign_button').hide();
+            $('#sign_success_badge').show();
+            $('#signature_warning').hide();
+        },
 
-            this.$signatureCheckbox.prop('disabled', true);
-
-            rpc.query({
-                route: '/signature_status',
-                params: { order_id: parseInt(orderId) },
-            }).then(data => {
-                this._setSignLink(data.sign_link || '');
-                if (data.signed) {
-                    this.$signatureCheckbox.prop('checked', true);
-                    $('#signature_warning').hide();
-                    $('#sign_button').hide();
-                } else {
-                    this.$signatureCheckbox.prop('checked', false);
-                    $('#signature_warning')
-                        .text("Debes firmar el documento antes de continuar.")
-                        .show();
-                    $('#sign_button').show();
-                }
-
-                this.$signatureCheckbox.prop('disabled', false);
-                this._updateSignatureDisabledReason();
-            }).catch(() => {
-                this.$signatureCheckbox.prop('disabled', false);
-                alert("Ocurrió un error al verificar la firma.");
-            });
+        _markAsUnsigned: function () {
+            this.$signatureSigned.val('0');
+            $('#sign_button').show();
+            $('#sign_success_badge').hide();
         },
 
         _setSignLink: function (signLink) {
@@ -104,8 +83,8 @@ odoo.define('isep_ecommerce_fix.signature_check', function (require) {
         _updateSignatureDisabledReason: function () {
             const disabledReasons = this.$submitButton.data('disabled_reasons') || {};
 
-            if (this.$signatureCheckbox.length > 0) {
-                disabledReasons.signature = !this.$signatureCheckbox.prop('checked');
+            if (this.$signButton.length > 0) {
+                disabledReasons.signature = this.$signatureSigned.val() !== '1';
             }
 
             this.$submitButton.data('disabled_reasons', disabledReasons);
@@ -117,7 +96,6 @@ odoo.define('isep_ecommerce_fix.signature_check', function (require) {
 
     checkoutForm.include(Object.assign({}, SignatureCheckMixin, {
         events: Object.assign({}, checkoutForm.prototype.events, {
-            'click #checkbox_signed': '_onClickSignatureCheckbox',
             'click #sign_button': '_onClickSignButton',
             'click #close_modal': '_onCloseModal',
             'click #sign_modal': '_onClickModalBackground',
@@ -137,10 +115,13 @@ odoo.define('isep_ecommerce_fix.signature_check', function (require) {
                 return;
             }
 
+            $('#sign_button').prop('disabled', true).html('<i class="fa fa-spinner fa-spin mr-1"/> Preparando documento...');
+
             rpc.query({
                 route: '/signature_prepare',
                 params: { order_id: parseInt(orderId) },
             }).then(data => {
+                $('#sign_button').prop('disabled', false).html('<i class="fa fa-pencil-square-o mr-1"/> Realizar firma de matrícula');
                 this._setSignLink(data.sign_link || '');
                 if (data.sign_link) {
                     this._openSignModal(data.sign_link);
@@ -148,6 +129,7 @@ odoo.define('isep_ecommerce_fix.signature_check', function (require) {
                 }
                 alert("No se pudo generar el documento de matrícula para firma.");
             }).catch(() => {
+                $('#sign_button').prop('disabled', false).html('<i class="fa fa-pencil-square-o mr-1"/> Realizar firma de matrícula');
                 alert("Ocurrió un error al preparar el documento de matrícula.");
             });
         },
@@ -157,6 +139,21 @@ odoo.define('isep_ecommerce_fix.signature_check', function (require) {
             $('#sign_iframe').attr('src', '');
             $('#loading_spinner').show();
             $('#sign_iframe').hide();
+
+            // After closing the modal, check if the document was signed
+            var self = this;
+            var orderId = this._getOrderId();
+            if (orderId) {
+                rpc.query({
+                    route: '/signature_status',
+                    params: { order_id: parseInt(orderId) },
+                }).then(function (data) {
+                    if (data.signed) {
+                        self._markAsSigned();
+                    }
+                    self._updateSignatureDisabledReason();
+                });
+            }
         },
 
         _onClickModalBackground: function (e) {
@@ -167,14 +164,14 @@ odoo.define('isep_ecommerce_fix.signature_check', function (require) {
 
         _onClickFallbackLink: function (e) {
             e.preventDefault();
-            const signLink = $('#sign_link_holder').val();
+            var signLink = $('#sign_link_holder').val();
             if (signLink) {
                 window.open(signLink, '_blank');
             }
         },
 
         _isButtonReady: function () {
-            const disabledReasons = this.$submitButton.data('disabled_reasons') || {};
+            var disabledReasons = this.$submitButton.data('disabled_reasons') || {};
             return !Object.values(disabledReasons).includes(true) && this._super(...arguments);
         },
     }));
