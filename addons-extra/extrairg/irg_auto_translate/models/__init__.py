70513c024abd2406f2a0db64aea7204e181dce64
Sebastian <soporte2@institutoraimongaja.com>
2026-03-25 11:16:26 +0100

    revert: remove irg_auto_translate and irg_language_nav modules (user rollback)

--- addons-extra/extrairg/irg_auto_translate/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import op_subject
-from . import auto_translate
-from . import translation_service
-from . import op_course
--- addons-extra/extrairg/irg_auto_translate/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Tests package for irg_auto_translate module."""
-
-from . import test_auto_translate
