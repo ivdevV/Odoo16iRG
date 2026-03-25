998f0f4ec451e46cc109bdd1eb0b849866387659
Sebastian <soporte2@institutoraimongaja.com>
2026-03-25 14:53:15 +0100

    fix: change VAT label to 'DNI/Pasaporte/Documento Identificativo' in certificates
    - certificate_templates.xml: update fallback label in template
    - irg_certificate_request.py: update fallback label in document generation model

--- addons-extra/extrairg/irg_gradebook_certificates/models/irg_certificate_request.py
+++ addons-extra/extrairg/irg_gradebook_certificates/models/irg_certificate_request.py
@@ -423,7 +423,7 @@ class IrgCertificateRequest(models.Model):
         id_label = (
             partner.l10n_latam_identification_type_id.name
             if partner.l10n_latam_identification_type_id
-            else 'DNI/Pasaporte'
+            else 'DNI/Pasaporte/Documento Identificativo'
         )
         documento = '%s %s' % (id_label, partner.vat or '')
 
