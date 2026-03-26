46bcbf3128e2b3e9b4efac20dc55a5a577381723
ivrogo <er_jotx@hotmail.com>
2026-03-26 10:13:58 +0100

    creado modulo para arreglar variable dni

---
+++ addons-extra/extrairg/irg_identification_types/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'IRG - Tipos de Identificación',
+    'summary': 'Crea los tipos de identificación estándar IRG: DNI, Pasaporte, Documento Identificativo',
+    'version': '16.0.1.0.0',
+    'category': 'Hidden',
+    'author': 'IRG',
+    'license': 'LGPL-3',
+    'depends': ['l10n_latam_base'],
+    'data': [
+        'data/identification_types.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
