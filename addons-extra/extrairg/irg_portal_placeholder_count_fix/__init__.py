1510d99e4032429b7d042d98dd08630ec6399fd9
matricula <soporte2@institutoraimongaja.com>
2026-04-17 10:40:23 +0200

    irg_portal_placeholder_count_fix: fix controller registration with monkey-patch

--- addons-extra/extrairg/irg_portal_placeholder_count_fix/__init__.py
+++ addons-extra/extrairg/irg_portal_placeholder_count_fix/__init__.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
 from . import controllers
+
+# Monkey-patch del controlador CustomerPortal para añadir valores por defecto a placeholders
+from odoo.addons.portal.controllers.portal import CustomerPortal
+from .controllers.portal import CustomerPortalPlaceholderCountFix
+
+# Reemplazar la clase original con la versión parcheada
+CustomerPortal._prepare_home_portal_values = CustomerPortalPlaceholderCountFix._prepare_home_portal_values
