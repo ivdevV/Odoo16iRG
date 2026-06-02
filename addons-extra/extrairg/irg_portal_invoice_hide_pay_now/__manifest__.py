6cbc723419fb841d02c3c11b1be7baf832c56eb7
ivdevV <soporte@institutoraimongaja.com>
2026-06-02 14:24:51 +0200

    feat: add module to hide Pay Now button from invoice portal

---
+++ addons-extra/extrairg/irg_portal_invoice_hide_pay_now/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'IRG Portal Invoice Hide Pay Now',
+    'version': '16.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Oculta los botones de "Pagar ahora" del portal de facturas',
+    'description': """
+        Elimina la opción de pago online del portal de facturas para todos los usuarios.
+        - Quita el botón "Pagar ahora" de la lista de facturas (/my/invoices)
+        - Quita el botón "Pagar ahora" del detalle de factura
+        - Quita el bloque de checkout de pago
+    """,
+    'author': 'iRG',
+    'website': '',
+    'depends': [
+        'account',
+        'account_payment',
+    ],
+    'data': [
+        'views/portal_templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': 'LGPL-3',
+}
