a5147c083b4eb1aed1ee65e60d98f3c5ed443a6e
ivdevV <soporte@institutoraimongaja.com>
2026-06-05 13:29:54 +0200

    fix(elearning): force bin size on slide onchange

---
+++ addons-extra/extrairg/irg_elearning_slide_onchange_bin_size/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'iRG eLearning Slide Onchange Bin Size',
+    'version': '16.0.1.0.0',
+    'summary': 'Evita cargar binarios reales durante onchange de diapositivas eLearning',
+    'category': 'Website/eLearning',
+    'author': 'iRG',
+    'license': 'LGPL-3',
+    'depends': [
+        'website_slides',
+        'irg_elearning_child_bin_size_fix',
+    ],
+    'installable': True,
+    'application': False,
+}
