486768ef70b77dce6de31a993dee1776602157e0
ivdevV <soporte@institutoraimongaja.com>
2026-06-23 11:49:35 +0200

    fix(partner): propagate gender changes to admissions and students

--- addons-extra/extrairg/irg_admission_gender_fix/models/res_partner.py
+++ addons-extra/extrairg/irg_admission_gender_fix/models/res_partner.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
@@ -14,3 +17,28 @@ class ResPartner(models.Model):
         ('female', 'Female'),
         ('not-sure', 'Not Sure')
     ], string='Género', default=False)
+
+    def write(self, vals):
+        res = super(ResPartner, self).write(vals)
+        if 'gender' in vals:
+            new_gender = vals['gender']
+            mapped = 'o'
+            if new_gender in ('m', 'male', 'Male', 'Masculino'):
+                mapped = 'm'
+            elif new_gender in ('f', 'female', 'Female', 'Femenino'):
+                mapped = 'f'
+            
+            _logger.info("IRG Gender Fix: Propagating partner gender change (%s -> %s) to admissions and students", new_gender, mapped)
+            for partner in self:
+                # Update admissions
+                admissions = self.env['op.admission'].sudo().search([('partner_id', '=', partner.id)])
+                if admissions:
+                    _logger.info("IRG Gender Fix: Updating gender to '%s' for %s admissions of partner %s", mapped, len(admissions), partner.name)
+                    admissions.write({'gender': mapped})
+                
+                # Update students
+                students = self.env['op.student'].sudo().search([('partner_id', '=', partner.id)])
+                if students:
+                    _logger.info("IRG Gender Fix: Updating gender to '%s' for %s students of partner %s", mapped, len(students), partner.name)
+                    students.write({'gender': mapped})
+        return res
