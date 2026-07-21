cfa660d292a521317deef0ad0b097dd2007787a7
ivdevV <soporte@institutoraimongaja.com>
2026-07-21 13:36:11 +0200

    feat(gradebook): scaffold del módulo irg_gradebook_moodle_wizard

---
+++ addons-extra/extrairg/irg_gradebook_moodle_wizard/models/__init__.py
@@ -0,0 +1,4 @@
+from . import moodle_map
+from . import app_gradebook_result
+from . import app_gradebook_student
+from . import gradebook_service
---
+++ addons-extra/extrairg/irg_gradebook_moodle_wizard/tests/__init__.py
@@ -0,0 +1,1 @@
+from . import test_moodle_sync_wizard
