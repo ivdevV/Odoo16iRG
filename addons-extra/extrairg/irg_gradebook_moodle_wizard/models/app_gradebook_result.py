669543b1b67fe05b8c60d8a5c4367d168b9b89df
ivdevV <soporte@institutoraimongaja.com>
2026-07-21 13:48:40 +0200

    feat(gradebook): is_moodle en resultados y acción de apertura del wizard

--- addons-extra/extrairg/irg_gradebook_moodle_wizard/models/app_gradebook_result.py
+++ addons-extra/extrairg/irg_gradebook_moodle_wizard/models/app_gradebook_result.py
@@ -1,1 +1,12 @@
+from odoo import fields, models
 
+
+class AppGradebookResult(models.Model):
+    _inherit = "app.gradebook.result"
+
+    is_moodle = fields.Boolean(
+        string="Origen Moodle",
+        default=False,
+        index=True,
+        help="Línea creada/actualizada por el wizard de sincronización Moodle.",
+    )
--- addons-extra/extrairg/irg_gradebook_moodle_wizard/models/app_gradebook_student.py
+++ addons-extra/extrairg/irg_gradebook_moodle_wizard/models/app_gradebook_student.py
@@ -1,1 +1,20 @@
+from odoo import _, models
 
+
+class AppGradebookStudent(models.Model):
+    _inherit = "app.gradebook.student"
+
+    def action_open_moodle_sync_wizard(self):
+        self.ensure_one()
+        wizard = self.env["irg.gradebook.moodle.sync.wizard"].create(
+            {"gradebook_student_id": self.id}
+        )
+        wizard.action_load_moodle_data()
+        return {
+            "type": "ir.actions.act_window",
+            "name": _("Sincronizar con Moodle"),
+            "res_model": "irg.gradebook.moodle.sync.wizard",
+            "res_id": wizard.id,
+            "view_mode": "form",
+            "target": "new",
+        }
